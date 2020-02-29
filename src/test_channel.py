08cd4de74fc8d9300d3903bc898da6fcdc1644ef
Yousif <z5236259@cse.unsw.edu.au>
2020-02-29 17:35:39 +1100

    started testing channel_leave

--- src/test_channel.py
+++ src/test_channel.py
@@ -7,7 +7,7 @@ import pytest
 def test_messages_empty_public(): 
     '''
     Under the assumption that auth_register, auth_login
-    channel_create, message_send work properly
+    channel_create, channel_invite, message_send work properly
     '''
     #creating user
     user_info = register("Yousif@unsw.com", "13131", "Yousif", "Khalid")
@@ -44,3 +44,9 @@ def test_messages_invalid_channel():
     owner_info = login("Yousif@unsw.com", "13131") 
     with pytest.raises(InputError):
         msgs = messages(owner_info['token'], 131123, 0)
+
+def test_messages_invalid_index():
+    #creating and logging in users
+    owner_info = login("Yousif@unsw.com", "13131")
+    user_info = register("member@unsw.com","12321", "John", "Wick") 
+    #creating private channel
