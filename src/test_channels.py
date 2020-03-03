1e6faa22aa2364952fece52e73bba6f376e78a6d
Ken <kenli9zy@gmail.com>
2020-03-03 23:32:32 +1100

    Added some preliminary tests for channels

--- src/test_channels.py
+++ src/test_channels.py
@@ -1,5 +1,31 @@
 #please edit this file for channels functions
+# TODO: import functions from other modules if needed
+import pytest
 import channels
+from error import InputError
 
-def test_channel_messages():
-    
+
+# Creating users
+# These users will be used for testing unless if more are added
+user_alice = auth_register("alice@gmail.com", "123456", "Alice", "Bee")
+user_charlie = auth_register("charlie@gmail.com", "ABCDEF", "Charlie", "Dragon")
+
+'''------------------testing channels_list--------------------'''
+
+# Testing whether channels_list returns empty list if no channels exist yet
+def test_channels_no_channels(): 
+    # Assuming users are not part of any channel after registration
+    assert channels_list(user_alice['token']) == []
+
+# Testing that channels_list only returns a channel if the user is part of it
+def test_channels_part_of_channel():
+    # Creating a public channel
+    channel_id = channels_create(user_alice['token'], 'channel_0', True)
+    assert channels_list(user_alice['token']) == [{'channel_id': channel_id, 'name': 'channel_0'}]
+    assert channels_list(user_charlie['token']) == []
+
+'''------------------testing channels_listall--------------------'''
+
+
+
+'''------------------testing channels_create--------------------'''
--- src/test_message.py
+++ src/test_message.py
@@ -1,1 +1,4 @@
 #please edit this file to test message functions
+import pytest
+import message
+from error import AccessError, InputError
