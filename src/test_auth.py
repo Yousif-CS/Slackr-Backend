eba3d69163b7c94156bc345f2e9bb28104d9e53a
z5205183 <z5205183@cse.unsw.edu.au>
2020-03-04 16:27:50 +1100

    test_auth.py

--- src/test_auth.py
+++ src/test_auth.py
@@ -1,1 +1,44 @@
-#please edit this file for auth functions tests
+import pytest 
+from auth import auth_register, auth_login, auth_logout 
+from error import InputError 
+
+#Test Valid Registration details 
+def test_register(): 
+	credentials = auth_register('max.smith@gmail.com', 'great_password101', 'Max', 'Smith') 
+
+#Invalid email 
+def test_register_invalid_email():
+	with pytest.raises(InputError) as e:
+		auth_register('maxsmith', 'cryptic_password#1500', 'Max', 'Smith')
+
+#Only domain given for email 
+def test_register_wrong_email():
+	with pytest.raises(InputError) as e:
+		auth_register('@gmail.com', 'passwordian', 'Max' 'Smith')
+
+#Password is too short 
+def test_register_invalid_password():
+	with pytest.raises(InputError) as e: 
+		auth_register('max.smith@gmail.com', 'abc', 'Max', 'Smith') 
+
+#First name is too many characters 
+def test_register_long_first_name():
+	with pytest.raises(InputError) as e: 
+		auth_register('max.smith@gmail.com', 'great_password101', 'c' * 51, 'Smith') 
+
+#Last name is too many characters 
+def test_register_long_last_name(): 
+	with pytest.raises(InputError) as e:
+		auth_register('max.smith@gmail.com', 'great_password101', 'Max', 'zz' * 26) 
+
+#User already exists 
+def test_register_existing_user(): 
+	original_user = auth_register('max.smith@gmail.com', 'great_password101', 'Max', 'Smith') 
+	
+	with pytest.raises(InputError) as e:
+		auth_register('max.smith@gmail.com', 'great_password101', 'Max', 'Smith') 
+		
+	
+	
+	 
+
