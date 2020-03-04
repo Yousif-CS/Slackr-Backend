df44a653a99117dbe863d7b8a1d1227aaecfc96e
Ken <kenli9zy@gmail.com>
2020-03-04 14:45:47 +1100

    Completed channels_list and channelst_listall

--- src/test_channels.py
+++ src/test_channels.py
@@ -21,8 +21,6 @@ user_cd = auth_register("charlie@gmail.com", "pw321ABC", "Charlie", "Dragon")
 # input: (token); output: {channels}
 # is_public status should not affect channnel
 
-
-
 # testing whether channels_list returns empty list if no channels exist yet
 def test_channels_list_no_channels(): 
     # creating and registering user (user_ab is type dictionary with u_id and token keys)
@@ -85,7 +83,53 @@ def test_channels_list_added_by_creator():
         ]
 
 '''------------------testing channels_listall--------------------'''
+# reminders
+# input: (token); output: {channels}
+# list every channel that exists (public and private)
+
+# testing that channels_listall returns an empty dictionary when no channels exist
+def test_channels_listall_empty():
+    # creating and registering user_ab
+    user_ab = auth_register("alice@gmail.com", "password11", "Alice", "Bee")
+    # assuming users are not part of any channel after registration
+    # and that no channels have been created
+    assert channels_listall(user_ab['token']) == {}
+
+# testing for a given public channel, channels_listall returns it for every user
+def test_channels_listall_public(): 
+    # creating and registering users
+    user_ab = auth_register("alice@gmail.com", "password11", "Alice", "Bee")
+    user_cd = auth_register("charlie@gmail.com", "pw321ABC", "Charlie", "Dragon")
+    # user_ab creates a public channel
+    new_public_channel = channels_create(user_ab['token'], 'public_test', True)
+    # user_ab and user_cd call channels_listall
+    ab_list = channels_list(user_ab['token'])
+    cd_list = channels_list(user_cd['token'])
+    assert ab_list['channels'] == cd_list['channels'] == \
+        [
+            {
+                'channel_id': 1
+                'name': 'public_test'
+            }
+        ]
 
 
+# testing for a private channel, channels_listall also returns it for every user
+def test_channels_listall_private():
+    # creating and registering users
+    user_ab = auth_register("alice@gmail.com", "password11", "Alice", "Bee")
+    user_cd = auth_register("charlie@gmail.com", "pw321ABC", "Charlie", "Dragon")
+    # user_cd creates a private channel
+    new_private_channel = channels_create(user_ab['token'], 'private_test', False)
+    # user_ab and user_cd call channels_listall
+    ab_list = channels_list(user_ab['token'])
+    cd_list = channels_list(user_cd['token'])
+    assert cd_list['channels'] == ab_list['channels'] == \
+        [
+            {
+                'channel_id': 1
+                'name': 'private_test'
+            }
+        ]
 
 '''------------------testing channels_create--------------------'''
