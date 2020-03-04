5084b69442ae5716b1cb2115f8d8ab918592998a
Joshua Wang <z5205511@ad.unsw.edu.au>
2020-03-04 17:08:15 +1100

    Added is_valid_email.py to src and imported it into test_user.py

--- src/test_user.py
+++ src/test_user.py
@@ -2,6 +2,7 @@ import pytest
 from error import InputError
 from user import user_profile, user_profile_setname, user_profile_setemail, user_profile_sethandle
 from auth import auth_register, auth_login
+from is_valid_email import is_valid_email
 
 
 # create users jwang and kli, and loging
@@ -56,12 +57,27 @@ def test_access_other_profiles(get_user_jwang, get_user_kli):
             }
         }
 
-# test that an invalid u_id (string) will throw an InputError
+    assert user_profile(jwang_token, kli_u_id) == \
+        {"user": {
+        	"u_id": kli_u_id,
+        	"email": "ken@gmail.com",
+        	"name_first": "Ken",
+        	"name_last": "Li",
+        	"handle_str": "kli",
+            }
+        }
+
+# test that an invalid u_id's will throw an InputError
 def test_user_profile_invalid_u_id(get_user_kli):
     kli_token, kli_u_id = get_user_kli
+    jwang_token, jwang_u_id = get_user_jwang
 
     with pytest.raises(InputError):
         user_profile(kli_token, "abc")
+        user_profile(jwang_token, "abc")
+        user_profile(kli_token, 2.75)
+        user_profile(jwang_token, 1.89)
+        user_profile(jwang_token, jwang_u_id + kli_u_id + 1)
 
 '''------------------testing user_profile_setname--------------------'''
 
@@ -85,7 +101,7 @@ def test_user_profile_setname(get_user_kli):
 # 2. name_last empty
 # 3. name_first over 50 char
 # 4. name_last over 50 char
-# 5. names that are purely whitespaces are counted as invalid
+# names that are purely whitespaces are counted as invalid
 def test_user_profile_setname_invalid(get_user_kli):
     kli_token, kli_u_id = get_user_kli
 
@@ -98,17 +114,27 @@ def test_user_profile_setname_invalid(get_user_kli):
         user_profile_setname(kli_token, "Ken", "        ")
 
 # test that authorised user can update their email address so long as it is valid and unique
-def test_user_profile_setemail():
-    
+def test_user_profile_setemail(get_user_kli):
+    kli_token, kli_u_id = get_user_kli
 
 # test that invalid emails throw input error
+def test_invalid_email(get_user_kli):
+    kli_token, kli_u_id = get_user_kli
+    pass
+
 # test that existing emails throw input error
+def test_nonunique_email(get_user_kli, get_user_jwang):
+    kli_token, kli_u_id = get_user_kli
+    jwang_token, jwang_u_id = get_user_jwang
+    pass
 
 # test that authorised user can update their handle so long as it is valid and unique (3 char and 20 char)
 def test_user_profile_sethandle():
 # test that handles too short <3 throw InputError
 # test that handles too long >20 throw InputError
 # test that non-unique handles throw InputError
+    pass
 
 def test_users_all():
     # tests that it returns a dictionary containing a dictionary of all the users
+    pass
