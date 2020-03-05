0eb53dffcca955d587fa7497be07f53f565e30ca
z5205183 <z5205183@cse.unsw.edu.au>
2020-03-05 17:15:58 +1100

    Logout tests added

--- src/test_auth.py
+++ src/test_auth.py
@@ -66,12 +66,67 @@ def test_login_no_user():
 
 	first_user = auth_register('max.smith@gmail.com', 'great_password101', 'Max', 'Smith')  
 	
-	scnd_user = auth_register('bob99@unsw.edu.au', '45&*ght', 'Bob', 'Johnson' 
+	scnd_user = auth_register('bob99@unsw.edu.au', '45&*ght', 'Bob', 'Johnson') 
 		
 	third_user = auth_register('kate58@bigpond.com', 'secret101', 'Kate' , 'Perkins') 
 	
 	with pytest.raises(InputError) as e:
 		auth_login('linda70@gmail.com' , 'let_me_in') 
+
+#Using user's token the logout is successful 
+def test_logout_success(): 
+	
+	user = auth_register('max.smith@gmail.com', 'great_password101', 'Max', 'Smith')  
+	
+	user_logging_in = auth_login('max.dsmith@gmail.com', 'great_password101') 
+
+	assert user['u_id'] == user_logging_in['u_id'] 
+	
+	user_token = user_logging_in['token'] 
+	message = auth_logout(user_token) 
+	
+	assert message['is_success'] == True 
+
+
+#Token is not valid for a user 
+def test_logout_unsuccessful(): 
+	user = auth_register('max.smith@gmail.com', 'great_password101', 'Max', 'Smith')  
+	
+	user_logging_in = auth_login('max.dsmith@gmail.com', 'great_password101') 
+	
+	assert user['u_id'] == user_logging_in['u_id'] 
+	user_token = user_logging_in['token'] 
+	
+	rand_token = 'Not a valid token'
+		
+	assert user_token != rand_token 
+	message = auth_logout(rand_token) 
+	assert message['is_success'] == False 
+	
+
+
+#A logged out user trying to join a channel 
+def test_logout_join_fails(): 
+	user = auth_register('max.smith@gmail.com', 'great_password101', 'Max', 'Smith')  
+	
+	user_logging_in = auth_login('max.dsmith@gmail.com', 'great_password101') 
+
+	assert user['u_id'] == user_logging_in['u_id'] 
+	
+	user_token = user_logging_in['token'] 
+	message = auth_logout(user_token) 
+	
+	assert message['is_success'] == True 
+	
+	with pytest.raises(InputError) as e: 
+		channel_join(user_token, 10)
+	
+		
+	
+	
+
+		
+	
 	
 	
 
