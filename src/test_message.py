9329ac26169655008d06189dbe86c555b77eac7f
Ken <kenli9zy@gmail.com>
2020-03-07 14:12:01 +1100

    Added invalid token tests in test_messages. Cleaned up style

--- src/test_message.py
+++ src/test_message.py
@@ -32,6 +32,7 @@ def create_private_channel(make_user_ab):
     channel_id = channels_create(user_ab['token'], 'test_channel_private', False) 
     return (channel_id, user_ab)
     
+
 '''------------------testing message_send--------------------'''
 # inputs: (token, channel_id, message); output: {message_id}
 
@@ -48,6 +49,7 @@ def test_message_send_correct_output(create_public_channel):
     assert first_msg['u_id'] == user_ab['u_id']
     assert first_msg['message'] == 'Hello world!'
 
+
 # check type of message_id to be int and that user messages' message_id's are unique 
 def test_message_send_type_id(create_public_channel):
     new_public_channel, user_ab = create_public_channel
@@ -57,6 +59,7 @@ def test_message_send_type_id(create_public_channel):
 
     assert type(msg1['message_id']) == type(msg2['message_id']) == int
     assert msg1['message_id'] != msg2['message_id']
+
 
 # testing that duplicate messages (messages with identical strings) are processed correctly
 def test_message_send_duplicate_msgs(create_public_channel, make_user_cd):
@@ -74,6 +77,7 @@ def test_message_send_duplicate_msgs(create_public_channel, make_user_cd):
     assert first_msg['message'] == second_msg['message']
     assert first_msg['message_id'] != second_msg['message_id']
     
+
 # testing that messages of any length (from 0 to 1000 characters) are allowed but any longer strings will throw InputError
 # 1. messages with white space and symbols, as long as length is under 1000 characters
 def test_message_send_msg_good(create_public_channel):
@@ -83,6 +87,7 @@ def test_message_send_msg_good(create_public_channel):
     msgs_view = channel_messages(user_ab['token'], new_public_channel['channel_id'], 0)
     # checking that all the above messages sent successfully
     assert len(msgs_view['messages']) == 2
+
 
 # testing for InputError when message length exceeds 1000 characters
 # 1. All symbols
@@ -103,6 +108,7 @@ def test_message_send_msg_bad_message(create_public_channel):
     with pytest.raises(InputError):
         message_send(user_ab['token'], new_public_channel['channel_id'], "message&3." * 500 + " " * 400 + "loong" * 50)
 
+
 # testing for AccessError: when user is not part of a channel 
 def test_message_send_not_part_of_channel(create_private_channel, make_user_cd):
     # user_ab creates a private channel but does not add user_cd to it
@@ -111,6 +117,7 @@ def test_message_send_not_part_of_channel(create_private_channel, make_user_cd):
 
     with pytest.raises(AccessError):
         message_send(user_cd['token'], new_private_channel['channel_id'], "fomo")
+
 
 # testing for AccesError: when previous user/owner of channel leaves the channel and attempts to send message
 def test_message_send_owner_removed_send(create_private_channel, make_user_cd):
@@ -132,6 +139,7 @@ def test_message_send_channel_not_found(make_user_ab):
     with pytest.raises(Exception):
         message_send(user_ab['token'], 22222, "Hello world!")
 
+
 # testing that empty strings as messages throws exception
 # assuming empty strings are bad
 def test_message_send_empty_string_bad(create_public_channel): 
@@ -140,16 +148,17 @@ def test_message_send_empty_string_bad(create_public_channel):
     with pytest.raises(Exception):
         message_send(user_ab['token'], new_public_channel['channel_id'], "")
 
+
+# testing that invalid token throws exception
+# assuming token with string 'invalid' is an invalid token
+def test_message_send_invalid_token(create_public_channel):
+    new_public_channel, user_ab = create_public_channel
+
+    with pytest.raises(Exception):
+        message_send("invalid", new_public_channel['channel_id'], "Bad message")
+
+
 '''------------------testing message_remove--------------------'''
-
-# TODO:
-# add assumptions: 
-# 1. Specs: InputError 'message (based on ID) no longer exists' i.e. refers to message_id being invalid
-# 2. Indexing: removing a message DOES NOT affect the indexing of the other messages
-# - reasoned from specs (e.g. message with ID of 3 removed; next time when attemping to remove message with ID 3 return error)
-# 3. 
-# testing for this function relies on correct functionality of message_send and channel_messages
-
 # no errors
 # testing that owner of channel can remove their own message 
 def test_message_remove_owner_remove_own(create_public_channel):
@@ -176,6 +185,7 @@ def test_message_remove_user_remove_own(create_private_channel, make_user_cd):
     msgs_view = channel_messages(user_ab['token'], new_private_channel['channel_id'], 0)
 
     assert len(msgs_view['messages']) == 2
+
 
 # testing that owner of channel can remove message of other users 
 # also testing with more messages
@@ -217,6 +227,7 @@ def test_message_remove_invalid_id(create_public_channel):
     with pytest.raises(InputError):
         message_remove(user_ab['token'], -1) 
 
+
 # AccessError
 # owner of channel removed as owner, tries to remove messages (now as user)
 def test_message_remove_old_owner(create_public_channel, make_user_cd):
@@ -245,3 +256,14 @@ def test_message_remove_user_remove_msg(create_public_channel, make_user_cd):
 
     with pytest.raises(AccessError):
         message_remove(user_cd['token'], msg2['message_id'])
+
+
+# testing that invalid token throws exception
+# assuming token with string 'invalid' is an invalid token
+def test_message_remove_invalid_token(create_public_channel):
+    new_public_channel, user_ab = create_public_channel
+    # user_ab and user_ef send messages to the channel
+    msg1 = message_send(user_ab['token'], new_public_channel['channel_id'], "Hello Elephant!")
+
+    with pytest.raises(Exception):
+        message_remove("invalid", msg1['message_id'])
