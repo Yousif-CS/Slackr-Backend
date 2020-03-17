9405778eb9f47b28bdefc43cbb2b24463a92b56e
Ken <kenli9zy@gmail.com>
2020-03-17 19:03:27 +1100

    Fixed message_send implementation

--- src/message.py
+++ src/message.py
@@ -4,11 +4,12 @@ from auth import verify_token
 from error import InputError, AccessError
 
 # TODO: check structure for TOKENS dictionary
+# TODO: import sched? need pip install
 
 def message_send(token, channel_id, message):
     '''
     input: valid token, channel_id to send message into, actual message string
-    output:
+    output: a globally unique message_id in dictionary
     '''
     # verify the user
     if verify_token(token) is False:
@@ -25,11 +26,15 @@ def message_send(token, channel_id, message):
     # checking channel_id is valid (user is part of)
     if channel_id not in data['Users'][u_id]['channels']:
         raise AccessError(description='You do not have access to send message in this channel')
-    # assigning new message_id
-    if len(data['Channels'][channel_id]['message']) == 0:
+    # assigning new message_id MUST BE GLOBALLY UNIQUE!
+    # starting from index 0
+    if len(data['Messages']) == 0:
         new_msg_id = 0
     else:
-        new_msg_id = max(data['Channels'][channel_id]['message']) + 1
+        id_list = []
+        for i in range(len(data['Messages'])):
+            id_list.append(data['Messages'][i]['message_id'])
+        new_msg_id = max(id_list) + 1
     # sending the actual message:
     # 1. append to list of message id's
     data['Channels'][channel_id]['message'].append(new_msg_id)
@@ -43,12 +48,24 @@ def message_send(token, channel_id, message):
         'reacts': {}
     })
 
+    return {'message_id': new_msg_id}
+
 
 def message_sendlater(token, channel_id, message, time_sent):
-    pass
+    '''
+    input: valid token, channel_id, message and time in the future
+    output: {message_id}; 
+    Sends message from user to channel (specified by id) at specific time, automatically
+    '''
 
+# TODO: how to know 
 def message_pin(token, message_id):
-    pass
+    '''
+    input: token, message_id
+    output: {}
+    Given message within a channel, mark it as 'pinned' 
+    '''
+    return {}
 
 def message_unpin(token, message_id):
     pass
