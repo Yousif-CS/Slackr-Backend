f6e8cc5fddb204155935ae07a8c9f89655528617
Joshua Wang <z5205511@ad.unsw.edu.au>
2020-03-18 14:03:01 +1100

    (Josh) implemented user_profile_sethandle, modified comments about the STORE dictionary in server.py to include a key for handle in the User part of the dictionary. I also cleaned up some stray and extraneous quotations marks in that comment. I made some slight modifications to user_test.py as the spec was updated to allow handles with as few as 2 characters (before it was 3).

--- src/user.py
+++ src/user.py
@@ -24,11 +24,11 @@ def user_profile(token, u_id):
 
     # check validity of user's token
     if verify_token(token) is False:
-        raise AccessError(description = "Invalid token")
+        raise AccessError(description="Invalid token")
 
     # check that the u_id of the user whose information the authorised user wants to access is valid
     if is_u_id_valid(u_id) is False:
-        raise InputError(description = "user id not valid")
+        raise InputError(description="user id not valid")
 
     # if so, return the corresponding information by retrieving from the database
     data = get_store()["Users"][u_id]
@@ -50,13 +50,13 @@ def user_profile_setname(token, name_first, name_last):
 
     # verify token is valid
     if verify_token(token) is False:
-        raise AccessError(description = "Invalid token")
+        raise AccessError(description="Invalid token")
 
     # verify that changes to name are allowed
     if len(name_first) < 1 or len(name_last) < 1 \
         or len(name_first) > 50 or len(name_last) > 50 \
         or name_first.isspace() or name_last.isspace():
-        raise InputError(description = "Names must be between 1 and 50 characters long inclusive, \
+        raise InputError(description="Names must be between 1 and 50 characters long inclusive, \
             and cannot contain exclusively whitespaces.")
 
     # modify name_first and name_last in the database as per the user's changes
@@ -64,3 +64,34 @@ def user_profile_setname(token, name_first, name_last):
     data = get_store()["Users"][u_id]
     data["name_first"] = name_first
     data["name_last"] = name_last
+
+def user_profile_sethandle(token, handle_str):
+    '''
+    input: token, handle_str
+    output: none
+    function: updates the authorised user's handle (display name) so long as it is valid
+    '''
+
+    # verify the token is valid
+    if verify_token(token) is False:
+        raise AccessError(description="Invalid token")
+
+    # verify the new handle_str is of the correct length
+    if len(handle_str) < 2:
+        raise InputError(description="handle_str too short - it must be between 2 and 20 characters inclusive")
+    if len(handle_str) > 20:
+        raise InputError(description="handle_str too long - - it must be between 2 and 20 characters inclusive")
+
+    # verify the new handle_str is unique
+    # allow the "change" if the authorised user's new handle_str is identical to their old one.
+    u_id = get_tokens[token]
+    data = get_store()["Users"]
+
+    if data[u_id]["handle"] != handle_str:
+        for identity in data:
+            if identity["handle"] == handle_str:
+                raise InputError(description="new handle_str not unique to this user")
+
+    # change the handle_str in the database
+    data[u_id]["handle"] = handle_str
+    
