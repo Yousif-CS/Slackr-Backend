291434eb154f48e19d4bbd63e6c09d21e6eaaa44
Ken <kenli9zy@gmail.com>
2020-03-20 11:16:17 +1100

    Implemented auth logout

--- src/auth.py
+++ src/auth.py
@@ -2,6 +2,8 @@
 This file contains implementations for authentication functions: register, login and logout, token generation
 along with helper functions used in other files as well
 '''
+from server import get_tokens
+from error import AccessError
 
 #Auth functions
 from server import get_tokens
@@ -26,3 +28,24 @@ def auth_register(email, password, name_first, name_last):
 
 def auth_login(email, password):
     return {'u_id': 0, 'token': '123123'}
+
+
+
+def auth_logout(token):
+    '''
+    input:valid token
+    output: is_success (True / False)
+    Must invalidate token to log user out.
+    '''
+    # verify the user
+    if verify_token(token) is False:
+        raise AccessError(description='Invalid token')
+
+    tokens = get_tokens()
+    u_id = get_tokens()[token]
+    # removing the key value pair from TOKENS
+    del tokens[token]
+    # checking if user is logged out
+    if get_token(u_id) is None:
+        return True
+    else: return False
