b93715e21d9cc93ddc5f7ad34e91b06f459e2290
Yousif <z5236259@cse.unsw.edu.au>
2020-03-24 02:52:27 +1100

    finished wrapping userpermission_change and fixed pylint in other.py

--- src/other_routes.py
+++ src/other_routes.py
@@ -2,5 +2,18 @@
 This module contains all the routes for miscellaneous functionalities
 '''
 
+import json
+from flask import request
+
 import other
 from server import APP
+
+@APP.route('/admin/userpermission/change', methods=['POST'])
+def u_per_change():
+    '''
+    A wrapper for userpermission_change
+    '''
+    payload = request.get_json()
+    other.userpermission_change(payload['token'], \
+        payload['u_id'], payload['permission_id'])
+    return json.dumps({})
