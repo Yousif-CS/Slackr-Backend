1dd29675e86b9623574e3e5cf958cf846feb26b4
Ken <kenli9zy@gmail.com>
2020-03-24 12:59:38 +1100

    Wrapped message_sendlater

--- src/message_routes.py
+++ src/message_routes.py
@@ -14,3 +14,13 @@ def send():
     data = request.get_json()
     response = message.message_send(data['token'], data['channel_id'], data['message'])
     return dumps(response)
+
+@APP.route("/message/sendlater", methods=['POST'])
+def sendlater():
+    '''
+    calls message_send from message
+    '''
+    data = request.get_json()
+    response = message.message_sendlater(data['token'], data['channel_id'],\
+        data['message'], data['time_sent'])
+    return dumps(response)
