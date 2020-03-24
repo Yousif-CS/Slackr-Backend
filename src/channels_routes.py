73726c42a5ac11ef0c9134e0f9257b8166f8f6c7
Ken <kenli9zy@gmail.com>
2020-03-24 12:49:52 +1100

    Wrapped channels_list

--- src/channels_routes.py
+++ src/channels_routes.py
@@ -2,5 +2,28 @@
 This module contains all the routes for channels functionalities
 '''
 
-import channels
+from json import dumps
+from flask import request
+from channels import channels_list, channels_listall, channels_create
 from server import APP
+
+@APP.route("/channels/list", methods=['GET'])
+def c_list():
+    '''
+    a route which calls channels_list
+    '''
+    data = request.get_json()
+    response = channels_list(data['token'])
+    return dumps(response)
+
+@APP.route("/channels/listall", methods=['GET'])
+def listall():
+    '''
+    a route which calls channels_listall
+    '''
+    data = request.get_json()
+    response = channels_listall(data['token'])
+    return dumps(response)
+
+
+
