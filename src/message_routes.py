aff341c620b8aded69b19debd943ba3cdfbc95cd
Ken <kenli9zy@gmail.com>
2020-03-24 12:57:52 +1100

    Wrapped message_send

--- src/message_routes.py
+++ src/message_routes.py
@@ -1,6 +1,16 @@
 '''
 This module contains all the routes for message functionalities
 '''
-
+from json import dumps
+from flask import request
 import message
 from server import APP
+
+@APP.route("/message/send", methods=['POST'])
+def send():
+    '''
+    calls message_send from message
+    '''
+    data = request.get_json()
+    response = message.message_send(data['token'], data['channel_id'], data['message'])
+    return dumps(response)
