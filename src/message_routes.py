b670477baa961a98d233576ee72705f8ca3c5110
Joshua Wang <z5205511@ad.unsw.edu.au>
2020-03-27 00:52:30 +1100

    Added to data checks to all wrappers

--- src/message_routes.py
+++ src/message_routes.py
@@ -5,6 +5,7 @@ from json import dumps
 from flask import request
 import message
 from server import APP
+from error import RequestError
 
 @APP.route("/message/send", methods=['POST'])
 def send():
@@ -12,6 +13,10 @@ def send():
     calls message_send from message
     '''
     data = request.get_json()
+
+    if not data["token"] or not data["channel_id"] or not data["message"]:
+        raise RequestError(description="Missing data in request body")
+
     response = message.message_send(data['token'], data['channel_id'], data['message'])
     return dumps(response)
 
@@ -21,6 +26,10 @@ def sendlater():
     calls message_send from message
     '''
     data = request.get_json()
+
+    if not data["token"] or not data["channel_id"] or not data["message"] or not data["time_sent"]:
+        raise RequestError(description="Missing data in request body")
+
     response = message.message_sendlater(data['token'], data['channel_id'],\
         data['message'], data['time_sent'])
     return dumps(response)
@@ -31,6 +40,10 @@ def react():
     a route that calls message_react from message
     '''
     data = request.get_json()
+
+    if not data["token"] or not data["message_id"] or not data["react_id"]:
+        raise RequestError(description="Missing data in request body")
+
     message.message_react(data['token'], data['message_id'], data['react_id'])
     return dumps({})
 
@@ -40,6 +53,10 @@ def unreact():
     a route that calls message_unreact from message
     '''
     data = request.get_json()
+
+    if not data["token"] or not data["message_id"] or not data["react_id"]:
+        raise RequestError(description="Missing data in request body")
+
     message.message_unreact(data['token'], data['message_id'], data['react_id'])
     return dumps({})
 
@@ -49,6 +66,10 @@ def pin():
     a route which calls message_pin from message
     '''
     data = request.get_json()
+
+    if not data["token"] or not data["message_id"]:
+        raise RequestError(description="Missing data in request body")
+
     message.message_pin(data['token'], data['message_id'])
     return dumps({})
 
@@ -58,6 +79,10 @@ def unpin():
     a route which calls message_pin from message
     '''
     data = request.get_json()
+
+    if not data["token"] or not data["message_id"]:
+        raise RequestError(description="Missing data in request body")
+
     message.message_unpin(data['token'], data['message_id'])
     return dumps({})
 
@@ -67,5 +92,22 @@ def delete():
     a route which calls message_delete from message
     '''
     data = request.get_json()
+
+    if not data["token"] or not data["message_id"]:
+        raise RequestError(description="Missing data in request body")
+
     message.message_remove(data['token'], data['message_id'])
+    return dumps({})
+
+@APP.route("/message/edit", methods=['PUT'])
+def edit():
+    '''
+    a route which calls message_edit from message
+    '''
+    data = request.get_json()
+
+    if not data["token"] or not data["message_id"] or not data["message"]:
+        raise RequestError(description="Missing data in request body")
+
+    message.message_edit(data["token"], data["message_id"], data["message"])
     return dumps({})
--- src/user_routes.py
+++ src/user_routes.py
@@ -6,6 +6,7 @@ import json
 import user
 from server import APP
 from flask import request
+from error import RequestError
 
 @APP.route('/user/profile', methods=['GET'])
 def profile():
@@ -13,6 +14,10 @@ def profile():
     A route to call user_profile
     '''
     payload = request.get_json()
+
+    if not payload["token"] or not payload["u_id"]:
+        raise RequestError(description="Missing data in request body")
+
     to_send = user.user_profile(payload["token"], payload["u_id"])
     return json.dumps(to_send)
 
@@ -22,6 +27,10 @@ def profile_setname():
     A route to call user_profile_setname
     '''
     payload = request.get_json()
+
+    if not payload["token"] or not payload["name_first"] or not payload["name_last"]:
+        raise RequestError(description="Missing data in request body")
+
     user.user_profile_setname(payload["token"], payload["name_first"], payload["name_last"])
     return json.dumps({})
 
@@ -31,6 +40,10 @@ def profile_setemail():
     A route to call user_profile_setemail
     '''
     payload = request.getjson()
+
+    if not payload["token"] or not payload["email"]:
+        raise RequestError(description="Missing data in request body")
+
     user.user_profile_setemail(payload["token"], payload["email"])
     return json.dumps({})
 
@@ -40,5 +53,10 @@ def profile_sethandle():
     A route to call user_profile_sethandle
     '''
     payload = request.getjson()
+
+    if not payload["token"] or not payload["handle_str"]:
+        raise RequestError(description="Missing data in request body")
+
     user.user_profile_sethandle(payload["token"], payload["handle_str"])
     return json.dumps({})
+    
