64bf3fa09edc6f5e9725254e21f3389c40f61c74
Ken <kenli9zy@gmail.com>
2020-03-27 11:42:13 +1100

    Added channels RequestError check

--- src/channels_routes.py
+++ src/channels_routes.py
@@ -4,8 +4,10 @@ This module contains all the routes for channels functionalities
 
 from json import dumps
 from flask import request
+
 from channels import channels_list, channels_listall, channels_create
 from server import APP
+from error import RequestError
 
 @APP.route("/channels/list", methods=['GET'])
 def c_list():
@@ -13,6 +15,10 @@ def c_list():
     a route which calls channels_list
     '''
     data = request.get_json()
+
+    if not data['token']:
+        raise RequestError(description="Missing data in request body")
+
     response = channels_list(data['token'])
     return dumps(response)
 
@@ -22,6 +28,10 @@ def listall():
     a route which calls channels_listall
     '''
     data = request.get_json()
+
+    if not data['token']:
+        raise RequestError(description="Missing data in request body")
+
     response = channels_listall(data['token'])
     return dumps(response)
 
@@ -31,6 +41,10 @@ def create():
     a route which calls channels_create
     '''
     data = request.get_json()
+
+    if not data['token'] or not data['name'] or data['is_public']:
+        raise RequestError(description="Missing data in request body")
+
     response = channels_create(data['token'], data['name'], data['is_public'])
     return dumps(response)
 
