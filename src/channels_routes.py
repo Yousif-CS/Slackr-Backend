0e95c44fef3adc7f97a4dad7a5cc04cc5cd32022
Yousif <yousef2000khalid@gmail.com>
2020-03-29 15:53:13 +1100

    fixed channels route raising exception about incorrect request

--- src/channels_routes.py
+++ src/channels_routes.py
@@ -43,7 +43,7 @@ def create():
     '''
     data = request.get_json()
 
-    if not data['token'] or not data['name'] or not data['is_public']:
+    if not data['token'] or not data['name'] or data['is_public'] is None:
         raise RequestError(description="Missing data in request body")
 
     response = channels_create(data['token'], data['name'], data['is_public'])
