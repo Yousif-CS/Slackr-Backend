a9c2ccde04adec17ca83e8f60ce187c13609171a
Ken <kenli9zy@gmail.com>
2020-03-29 19:30:03 +1100

    Finished channels http testing

--- src/http_channels_test.py
+++ src/http_channels_test.py
@@ -8,19 +8,49 @@ from http_helpers import reset, register, login, logout, channels_list, \
     channels_create, channels_listall
 
 
+# start testing on channels_create
+def test_channels_create_ok(reset):
+    '''
+    Testing creating a channel with correct inputs functions correctly
+    '''
+    a_id, a_token = register('admin@gmail.com', 'pass123456', 'Alan', 'Brown')
+    channels_create(a_token, 'test_public', True)
+    payload = channels_list(a_token)[0]
+    # asserting
+    assert payload['channel_id'] == 1
+    assert payload['name'] == 'test_public'
+    logout(a_token)
+
+def test_channels_create_bad():
+    '''
+    Testing invalid inputs (e.g. name too long) will be picked up and thrown error
+    '''
+    a_id, a_token = login('admin@gmail.com', 'pass123456')
+    with pytest.raises(HTTPError):
+        channels_create(a_token, 225, True)
+
+def test_channels_create_invalid_token():
+    '''
+    Testing valid token when attempting to create channel
+    '''
+    a_id, a_token = login('admin@gmail.com', 'pass123456')
+    with pytest.raises(HTTPError):
+        channels_create(a_token + 'bad', 'test_name', True)
+
+
 def test_channels_list_empty(reset):
     '''
-    no channels created yet 
+    no channels created yet
     '''
     a_id, a_token = register('admin@gmail.com', 'pass123456', 'Alan', 'Brown')
     payload = channels_list(a_token)
-    # check the list is empty 
+    # check the list is empty
     assert payload == []
     logout(a_token)
 
 def test_channels_list_ok():
     '''
-    single channel created by admin 
+    single channel created by admin
     '''
     a_id, a_token = login('admin@gmail.com', 'pass123456')
     channels_create(a_token, 'test_public', True)
@@ -42,8 +72,8 @@ def test_channels_list_multiple():
     channels_create(a_token, 'test_priv', False)
     channels_create(k_token, 'ken_channel', True)
     # checking that all channels are added
-    assert channels_list(a_token) == \
-        [{
+    assert channels_list(a_token) == [
+        {
             'channel_id': 1,
             'name': 'test_public',
         },
@@ -59,17 +89,23 @@ def test_channels_list_multiple():
     logout(a_id)
 
 def test_channels_list_invalid_token():
+    '''
+    Testing invalid token
+    '''
     a_id, a_token = login('admin@gmail.com', 'pass123456')
     # chuck in invalid token
     with pytest.raises(HTTPError):
         channels_list(a_token + 'invalid')
 
 def test_channels_listall_ok():
+    '''
+    Testing listing all channels when there are multiple
+    '''
     a_id, a_token = login('admin@gmail.com', 'pass123456')
     k_id, k_token = login('ken@gmail.com', 'kenisaperson')
     # assert both users get the same list when calling listall
-    assert channels_listall(a_token) == channels_listall(k_token) == \
-        [{
+    assert channels_listall(a_token) == channels_listall(k_token) == [
+        {
             'channel_id': 1,
             'name': 'test_public',
         },
@@ -83,15 +119,23 @@ def test_channels_listall_ok():
         }]
     logout(a_token)
     logout(k_token)
-    
+  
 def test_channels_listall_empty(reset):
+    '''
+    Testing return of empty payload when no channels exist
+    '''
     a_id, a_token = register('admin@gmail.com', 'pass123456', 'Alan', 'Brown')
-    
+ 
     assert channels_listall(a_token) == []
     logout(a_token)
 
 def test_channels_listall_invalid_token():
+    '''
+    Testing invalid token
+    '''
     a_id, a_token = login('admin@gmail.com', 'pass123456')
     # chuck in invalid token
     with pytest.raises(HTTPError):
         channels_listall(a_token + 'invalid')
+
+
