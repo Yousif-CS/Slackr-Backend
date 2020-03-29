837872cf7b707a2b582c33f57cdda625665f0248
z5205183 <z5205183@cse.unsw.edu.au>
2020-03-29 17:45:34 +1100

    GET http methods in standup_routes fixed

--- src/standup_routes.py
+++ src/standup_routes.py
@@ -30,12 +30,13 @@ def st_active():
     '''
     Wrapper for standup_active
     '''
-    payload = request.get_json()
+    token = request.args.get('token')
+    channel_id = request.args.get('channel_id')
 
-    if not payload['token'] or not payload['channel_id']:
+    if not token or not channel_id:
         raise RequestError(description=f"Missing data in request body")
 
-    to_send = standup.standup_active(payload['token'], payload['channel_id'])
+    to_send = standup.standup_active(token, int(channel_id))
 
     return json.dumps(to_send)
 
