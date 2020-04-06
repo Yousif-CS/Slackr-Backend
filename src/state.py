193fc83a99b17d48f07a5b55f6ad884031a460b7
Yousif <yousef2000khalid@gmail.com>
2020-04-06 19:30:35 +1000

    added a stub for the database classes that will be used to refactor

--- src/state.py
+++ src/state.py
@@ -6,6 +6,8 @@ from threading import Timer, Thread
 import pickle
 import time
 
+from error import InputError, AccessError
+
 # This dictionary will contain all of the database
 # once the server starts and we unpickle the database file
 # it is supposed to be {
@@ -43,7 +45,164 @@ import time
 #                                                  }, ...]
 #                                   }]
 #                   }
-STORE = dict()
+# STORE = dict()
+
+
+class Users():
+    def __init__(self):
+        self._users = dict()
+        self._permissions = [1, 2]
+        self._num_users = 0
+    def add_user(self, email, password, f_name, l_name):
+        '''
+        adds user with given details to the database
+        Arguments: first and last names, email, password, handle and permissions
+        Returns: None
+        Raises: InputError, AccessError
+        '''
+        if self.email_used(email):
+            raise InputError("Email already used")
+
+        if self._num_users == 0:
+            
+
+    def user_details(self, u_id):
+        pass
+
+    def all_users(self):
+        pass
+
+    def user_exists(self, u_id):
+        pass
+    def email_used(self, email):
+        pass
+
+class Admins(Users):
+    def __init__(self):
+        pass
+    def add_admin(self, u_id):
+        pass
+
+    def is_admin(self, u_id):
+        pass
+
+class Channels():
+    def __init__(self):
+        self._channels = dict()
+    def add(self, details):
+        pass
+    
+    def join(self, channel_id, u_id):
+        pass
+    
+    def leave(self, channel_id, u_id):
+        pass
+    
+    def channel_exists(self, channel_id):
+        pass
+
+    def all(self):
+        pass
+
+    def is_member(self, channel_id, u_id):
+        pass
+
+    def is_owner(self, channel_id, u_id):
+        pass
+
+    def owners(self, channel_id):
+        pass
+
+    def members(self, channel_id):
+        pass
+
+class Messages():
+    def __init__(self):
+        self._messages = list()
+        self._react_ids = [1]
+        
+    def add(self, details):
+        pass
+
+    def remove(self, message_id):
+        pass
+
+    def find(self, message_id):
+        pass
+
+    def search(self, query_string):
+        pass
+
+    def is_valid_react(self, react_id):
+        pass
+
+    def is_sender(self, message_id, u_id):
+        pass
+
+class User_Message():
+    '''
+    Contains a structure that maintains the relationship
+    between users, channels and messages they sent
+    '''
+    def __init__(self):
+        self._user_messages = list()
+
+    def add_link(self, user_id, channel_id, message_id):
+        pass
+
+    def remove_link_by_user(self, user_id):
+        pass
+
+    def remove_link_by_channel(self, channel_id):
+        pass
+    
+    def remove_link_by_message(self, channel_id):
+        pass
+
+class User_Channel():
+    '''
+    Contains a structure that maintains the relationship
+    between users and the channels they have joined
+    '''
+    def __init__(self):
+        self._user_channels = list()
+
+    def add_link(self, user_id, channel_id):
+        pass
+
+    def remove_link_by_user(self, user_id):
+        pass
+
+    def remove_link_by_channel(self, channel_id):
+        pass
+
+class Database():
+    def __init__(self):
+        self.Users = Users()
+        self.Admins = Admins()
+        self.Channels = Channels()
+        self.Messages = Messages()
+        self.User_Message = User_Message()
+        self.User_Channel = User_Channel()
+
+    def add_admin(self, u_id):
+        pass
+    def remove_admin(self, u_id):
+        pass
+    def add_channel(self, u_id, channel_id, is_public):
+        pass
+
+    def join_channel(self, u_id, channel_id):
+        pass
+
+    def leave_channel(self, u_id, channel_id):
+        pass
+
+    def add_message(self, channel_id, message):
+        pass
+
+    def remove_message(self, message_id):
+        pass
 
 # this dictionary contains the session tokens that
 # won't need to be stored in the Store data dictionary for pickling
