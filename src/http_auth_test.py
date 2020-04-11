2030cc634245b4f62d42f56e6cfd57a6ed9b8127
Yousif <yousef2000khalid@gmail.com>
2020-04-11 14:13:12 +1000

    fixed http_auth_tests after stopping user from loggin in twice

--- src/http_auth_test.py
+++ src/http_auth_test.py
@@ -14,8 +14,8 @@ from http_helpers import (reset, register, login, logout, channels_create, chann
 
 def test_login_user(reset):
     reg_token = register('max.d@gmail.com', 'wubbalubba', 'Max', 'Smith')[1]
+    logout(reg_token)
     login_token = login('max.d@gmail.com', 'wubbalubba')[1]
-
     assert reg_token == login_token 
     success = logout(login_token)
     assert success == True 
@@ -23,19 +23,23 @@ def test_login_user(reset):
 
 def test_login_multiple_users(reset):
     reg_token = register('max.d@gmail.com', 'wubbalubba', 'Max', 'Smith')[1]
+    logout(reg_token)
     log_token = login('max.d@gmail.com', 'wubbalubba')[1]   
     logout(log_token)
 
     reg_token = register('bob@gmail.com', 'wubbalubba', 'Bob', 'Smith')[1]
+    logout(reg_token)
     log_token = login('bob@gmail.com', 'wubbalubba')[1]
     logout(log_token)
 
     reg_token = register('mike@gmail.com', 'wubbalubba', 'Mike', 'Smith')[1]
+    logout(reg_token)
     log_token = login('mike@gmail.com', 'wubbalubba')[1]
     logout(log_token)
 
 def test_register_without_logging_out(reset):
     reg_token = register('max.d@gmail.com', 'wubbalubba', 'Max', 'Smith')[1]
+    logout(reg_token)
     log_token = login('max.d@gmail.com', 'wubbalubba')[1]   
     assert log_token == reg_token 
     reg_token = register('max2.d@gmail.com', 'wubbalubba', 'Max', 'Smith')
@@ -78,13 +82,15 @@ def test_invalid_email(reset):
         register('max.smith', 'wubbalubba', 'Max', 'Smith')
     
 def test_join_channel_logged_out(reset):
-    register('max.d@gmail.com', 'wubbalubba', 'Max', 'Smith')
+    _, reg_token = register('max.d@gmail.com', 'wubbalubba', 'Max', 'Smith')
+    logout(reg_token)
     owner_token = login('max.d@gmail.com', 'wubbalubba')[1]
     channel_id = channels_create(owner_token, 'Maxs Channel', is_public=True)
     success = logout(owner_token)
     assert success == True 
 
-    register('mike@gmail.com', 'wubbalubba', 'Mike', 'Smith')[1]
+    _, reg_token = register('mike@gmail.com', 'wubbalubba', 'Mike', 'Smith')
+    logout(reg_token)
     user_token = login('mike@gmail.com', 'wubbalubba')[1]
     logout(user_token)
 
